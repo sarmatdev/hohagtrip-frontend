d26c17085d2b8e7e0e141eaf4cb36838f30b7e2c
Sarmat Khachirov <skhachirov12@gmail.com>
2021-05-09 12:17:38 +0300

    feat: auth mvp

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,13 @@
-import { createStore } from 'vuex'
+import { createStore, createLogger } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import auth from './modules/auth'
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    auth,
+  },
+  plugins: [createPersistedState(), createLogger()],
 })
