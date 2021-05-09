67578cfdc61d84e14cb9eee8a73e5be4acded1a0
Sarmat Khachirov <skhachirov12@gmail.com>
2021-05-09 12:18:33 +0300

    feat: profile page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import store from '../store'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -16,11 +17,29 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Login',
     component: () => import('../views/Login.vue'),
   },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: () => import('../views/Profile.vue'),
+    meta: { requiresAuth: true },
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && store.getters['auth/isAuth']) {
+    if (store.getters['auth/isAuth']) {
+      next()
+      return
+    }
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
