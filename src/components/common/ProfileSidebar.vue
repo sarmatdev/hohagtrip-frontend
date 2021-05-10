69b1002b09c8619bbd5599996ceb845972a5cbcc
Sarmat Khachirov <skhachirov12@gmail.com>
2021-05-10 21:23:07 +0300

    feat[wip]: profile sidebar component

---
+++ src/components/common/ProfileSidebar.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="flex flex-col flex-grow border-r border-gray-200 pt-5 pb-4 bg-white overflow-y-auto"
+  >
+    <div class="flex-grow flex flex-col">
+      <nav class="flex-1 px-2 bg-white" aria-label="Sidebar">
+        <a
+          v-for="item in navigation"
+          :key="item.name"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-100 text-gray-900'
+              : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+            'group flex items-center px-2 py-2 text-sm font-medium rounded-md',
+          ]"
+        >
+          <component
+            :is="item.icon"
+            :class="[
+              item.current
+                ? 'text-gray-500'
+                : 'text-gray-400 group-hover:text-gray-500',
+              'mr-3 h-6 w-6',
+            ]"
+            aria-hidden="true"
+          />
+          {{ item.name }}
+          <span
+            v-if="item.count"
+            :class="[
+              item.current ? 'bg-white' : 'bg-gray-100 group-hover:bg-gray-200',
+              'ml-auto inline-block py-0.5 px-3 text-xs font-medium rounded-full',
+            ]"
+          >
+            {{ item.count }}
+          </span>
+        </a>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  FolderIcon,
+  HomeIcon,
+  InboxIcon,
+  UsersIcon,
+} from '@heroicons/vue/outline'
+
+const navigation = [
+  { name: 'Dashboard', icon: HomeIcon, href: '#', current: true },
+  { name: 'Team', icon: UsersIcon, href: '#', count: 3, current: false },
+  { name: 'Projects', icon: FolderIcon, href: '#', count: 4, current: false },
+  { name: 'Calendar', icon: CalendarIcon, href: '#', current: false },
+  { name: 'Documents', icon: InboxIcon, href: '#', count: 12, current: false },
+  { name: 'Reports', icon: ChartBarIcon, href: '#', current: false },
+]
+
+export default defineComponent({
+  setup() {
+    return {
+      navigation,
+    }
+  },
+})
+</script>
+
+<style></style>
