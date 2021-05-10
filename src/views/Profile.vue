42e2b10995743181fad579b099fca9e3737531e0
Sarmat Khachirov <skhachirov12@gmail.com>
2021-05-10 16:55:39 +0300

    merge

---
+++ src/views/Profile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="profile">
+    <aside class="navbar">navbar</aside>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { defineComponent } from 'vue'
+import { useStore } from 'vuex'
+export default defineComponent({
+  setup() {
+    const store = useStore()
+
+    return {
+      store,
+    }
+  },
+  computed: {
+    user() {
+      // @ts-ignore
+      return this.store.getters['auth/user']
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+.profile {
+  .navbar {
+    @apply text-gray-800;
+  }
+}
+</style>
