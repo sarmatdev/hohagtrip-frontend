f223e229bdc8c940b4626df2913c3e0bb99e5371
sarmat <skhachirov12@gmail.com>
2021-07-03 14:00:15 +0300

    fix: add z-index

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,5 +1,9 @@
 <template>
-  <Disclosure as="nav" class="bg-white shadow fixed w-full" v-slot="{ open }">
+  <Disclosure
+    as="nav"
+    class="bg-white shadow fixed w-full z-40"
+    v-slot="{ open }"
+  >
     <div class="max-w-screen-xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex justify-between h-16">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
