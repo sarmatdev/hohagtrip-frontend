4641a1a014edde6f7a95c3783895d01b56e296a4
sarmat <skhachirov12@gmail.com>
2021-07-06 14:01:48 +0300

    feat: base icon component

---
+++ src/components/base/BaseIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <component v-bind="$attrs" :is="name" class="inline" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import * as Icons from '@heroicons/vue/outline'
+import { camelCase, upperFirst } from 'lodash-es'
+export default defineComponent({
+  name: 'BaseIcon',
+  components: {
+    ...Icons,
+  },
+  props: {
+    icon: String,
+  },
+  computed: {
+    name(): string {
+      return `${upperFirst(camelCase(this.icon))}Icon`
+    },
+  },
+})
+</script>
+
+<style></style>
