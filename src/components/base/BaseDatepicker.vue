96481f22894b67d00cfae16626928cd514ecf09a
sarmat <skhachirov12@gmail.com>
2021-07-06 14:02:02 +0300

    feat: base datepicker component

---
+++ src/components/base/BaseDatepicker.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="base-datepicker">
+    <label class="base-datepicker__label">{{ label }}</label>
+    <litepie-datepicker
+      class="mt-1"
+      v-model="dateValue"
+      i18n="ru"
+      as-single
+      :formatter="formatter"
+    ></litepie-datepicker>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import LitepieDatepicker from 'litepie-datepicker'
+
+export default defineComponent({
+  props: {
+    label: String,
+  },
+  components: {
+    LitepieDatepicker,
+  },
+  setup() {
+    const dateValue = ref([])
+    const formatter = ref({
+      date: 'DD MMM YYYY',
+      month: 'MMM',
+    })
+
+    return {
+      dateValue,
+      formatter,
+    }
+  },
+})
+</script>
+
+<style lang="scss">
+.base-datepicker {
+  @apply flex flex-col;
+
+  &__label {
+    @apply block text-sm font-medium text-gray-700 ml-2;
+  }
+}
+</style>
